b991c40f0c79fbc577c83156db3fa348184a963a
antvolkov84 <antvolkov84@gmail.com>
2024-04-05 20:05:32 +0300

    model User and auth

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
+import { validationResult } from 'express-validator';
+
+import { registerValidation } from './validations/auth.js';
+
+import UserModel from './models/User.js';
 
 mongoose
   .connect(
-    'mongodb+srv://antvolkov84:j0Pal1thWa5XtHwS@cluster0.1t50rt5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    'mongodb+srv://antvolkov84:j0Pal1thWa5XtHwS@cluster0.1t50rt5.mongodb.net/check?retryWrites=true&w=majority&appName=Cluster0'
   )
   .then(() => console.log('MongoDB OK!'))
   .catch((error) => console.log(error));
@@ -30,7 +36,42 @@ app.post('/auth/login', (req, res) => {
   });
 });
 
-app.post('/auth/register', (req, res) => {});
+app.post('/auth/register', registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(8);
+    const hash = await bcrypt.hash(password, salt);
+    const doc = new UserModel({
+      email: req.body.email,
+      fullname: req.body.fullname,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret12345',
+      {
+        expiresIn: '30d',
+      }
+    );
+    const { passwordHash, ...userData } = user._doc;
+    res.json({ ...userData, token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Не удалось зарегистрироваться!',
+    });
+  }
+});
 
 app.listen(3333, (error) => {
   if (error) {
---
+++ server/validations/auth.js
@@ -0,0 +1,10 @@
+import { body } from 'express-validator';
+
+export const registerValidation = [
+  body('email', 'Неверный формат почты').isEmail(),
+  body('password', 'Пароль должен быть минимум 5 символов').isLength({
+    min: 5,
+  }),
+  body('fullname', 'Имя и фамилию, пожалуйста').isLength({ min: 3 }),
+  body('avatarUrl', 'Неверная ссылка на аватарку').optional().isURL(),
+];
