60b8617b0fc9b1cd4f41e37d2a4817a087d163c5
antvolkov84 <antvolkov84@gmail.com>
2024-04-06 20:41:01 +0300

    backend complete at now!

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import multer from 'multer';
 
 import { registerValidation, loginValidation } from './validations/auth.js';
 import { productValidation } from './validations/products.js';
 
 import * as UserController from './controllers/UserController.js';
 import * as ProductsController from './controllers/ProductsController.js';
+import handleValidationErrors from './utils/handleValidationErrors.js';
 
 import checkAuth from './utils/checkAuth.js';
 
@@ -17,7 +19,26 @@ mongoose
   .catch((error) => console.log(error));
 
 const app = express();
+
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, 'uploads');
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
 app.use(express.json());
+
+app.use('/uploads', express.static('uploads'));
 
 app.get('/', (req, res) => {
   res.send('Hello world');
@@ -27,14 +48,34 @@ app.get('/', (req, res) => {
 
 app.get('/products', ProductsController.getAll);
 app.get('/products/:id', ProductsController.getOne);
-app.post('/products', productValidation, ProductsController.addProduct);
+app.post(
+  '/products',
+  productValidation,
+  handleValidationErrors,
+  ProductsController.addProduct
+);
 app.delete('/products/:id', ProductsController.remove);
-app.patch('/products/:id', ProductsController.update);
+app.patch(
+  '/products/:id',
+  productValidation,
+  handleValidationErrors,
+  ProductsController.update
+);
 
 // Логин
-app.post('/auth/login', loginValidation, UserController.login);
+app.post(
+  '/auth/login',
+  loginValidation,
+  handleValidationErrors,
+  UserController.login
+);
 app.get('/auth/me', checkAuth, UserController.authMe);
-app.post('/auth/register', registerValidation, UserController.register);
+app.post(
+  '/auth/register',
+  registerValidation,
+  handleValidationErrors,
+  UserController.register
+);
 
 app.listen(3333, (error) => {
   if (error) {
