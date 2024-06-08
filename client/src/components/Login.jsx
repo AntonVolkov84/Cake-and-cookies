edef41bafdad8d52d770eacdc95a76c6ce7b5645
antvolkov84 <antvolkov84@gmail.com>
2024-06-08 12:49:19 +0300

    finish login page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -5,10 +5,20 @@ import './Login.scss';
 
 function Login() {
   return (
-    <main className="main_menu">
-      <TimantanLong />
-      <ExitShort />
-    </main>
+    <>
+      <main className="main_menu">
+        <TimantanLong />
+        <div className="blocklogin">
+          <div className="blocklogin_container">
+            <div className="blocklogin_text">Логин:</div>
+            <input className="blocklogin_input" placeholder="Логин"></input>
+            <div className="blocklogin_text">Пароль:</div>
+            <input className="blocklogin_input" placeholder="Пароль"></input>
+          </div>
+        </div>
+        <ExitShort />
+      </main>
+    </>
   );
 }
 
