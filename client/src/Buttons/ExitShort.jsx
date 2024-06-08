d5621e55f0230cc838fc91064bd80611e23079eb
antvolkov84 <antvolkov84@gmail.com>
2024-06-08 11:37:30 +0300

    refact to mixin and vars for Home/

---
+++ client/src/Buttons/ExitShort.jsx
@@ -0,0 +1,7 @@
+function ExitShort() {
+  return (
+    <button className="exitShort">Выход</button>
+  )
+}
+
+export default ExitShort
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,5 @@
+function Login() {
+  return <div>Loginsadas</div>;
+}
+
+export default Login;
