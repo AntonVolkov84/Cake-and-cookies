c0bb1c9f17768a20bc336d04983960d0d9572817
antvolkov84 <antvolkov84@gmail.com>
2024-06-08 14:17:28 +0300

    finished Wronglogin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home';
 import Login from './components/Login';
+import Wronglogin from './components/Wronglogin';
 
 function App() {
   return (
     <Routes>
       <Route path="" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/wronglogin" element={<Wronglogin />} />
     </Routes>
   );
 }
---
+++ client/src/components/Wronglogin.jsx
@@ -0,0 +1,25 @@
+import ExitShort from '../Buttons/ExitShort';
+import TimantanLong from '../Buttons/TimantanLong';
+
+import './Wronglogin.scss';
+
+function Wronglogin() {
+  return (
+    <>
+      <main className="main_menu">
+        <TimantanLong />
+        <div className="wrongblocklogin">
+          <div className="wrongblocklogin_container">
+            <span className="wrongblocklogin_text">
+              Неверный логин или пароль
+            </span>
+            <button className="wrongblocklogin_btn">Попробовать еще</button>
+          </div>
+        </div>
+        <ExitShort />
+      </main>
+    </>
+  );
+}
+
+export default Wronglogin;
